fb2506e4f51746d8675d6e9f38a07239c9c5f2fd
Anton Surkis <antonsurkis@gmail.com>
2020-09-17 20:53:10 +0300

    DFS

--- main.pl
+++ main.pl
@@ -38,6 +38,80 @@ distance( yaroslavl     , minsk           ,  940 ).
 distance( ufa           , kazan           ,  525 ).
 distance( ufa           , samara          ,  461 ).
 
+/* Если задавать правило инверсии, 
+   то будет бесконечный поиск */
+distance( brest           , vilnius       , 531 ).
+distance( brest           , vitebsk       , 638 ).
+distance( vilnius         , vitebsk       , 360 ).
+distance( vitebsk         , voronezh      , 869 ).
+distance( volgograd       , voronezh      , 581 ).
+distance( vitebsk         , volgograd     , 455 ).
+distance( nizhny_novgorod , vitebsk       , 911 ).
+distance( daugavpils      , vilnius       , 211 ).
+distance( brest           , kaliningrad   , 699 ).
+distance( vilnius         , kaliningrad   , 333 ).
+distance( vilnius         , kaunas        , 102 ).
+distance( vilnius         , kyiv          , 734 ).
+distance( zhytomyr        , kyiv          , 131 ).
+distance( donetsk         , zhytomyr      , 863 ).
+distance( volgograd       , zhytomyr      , 493 ).
+distance( kyiv            , kishinev      , 467 ).
+distance( donetsk         , kishinev      , 812 ).
+distance( vitebsk         , st_petersburg , 602 ).
+distance( kaliningrad     , st_petersburg , 739 ).
+distance( riga            , st_petersburg , 641 ).
+distance( kazan           , moscow        , 815 ).
+distance( nizhny_novgorod , moscow        , 411 ).
+distance( minsk           , moscow        , 690 ).
+distance( donetsk         , moscow        , 084 ).
+distance( st_petersburg   , moscow        , 664 ).
+distance( st_petersburg   , murmansk      , 412 ).
+distance( minsk           , murmansk      , 238 ).
+distance( vitebsk         , oryol         , 522 ).
+distance( donetsk         , oryol         , 709 ).
+distance( moscow          , oryol         , 368 ).
+distance( kyiv            , odessa        , 487 ).
+distance( kaunas          , riga          , 267 ).
+distance( riga            , tallinn       , 308 ).
+distance( kyiv            , kharkiv       , 471 ).
+distance( simferopol      , kharkiv       , 639 ).
+distance( voronezh        , yaroslavl     , 739 ).
+distance( minsk           , yaroslavl     , 940 ).
+distance( kazan           , ufa           , 525 ).
+distance( samara          , ufa           , 461 ).
+
+indent(0).
+indent(I) :-
+  write('  '),
+  succ(I1, I),
+  indent(I1).
+
+dfs(A, A, V, [A], 0) :-
+  length(V, I), 
+  indent(I), 
+  write('Found solution!\n').
+dfs(A, B, V, [A|P], S) :-
+  distance(A, C, Sd),
+  not(member(C, V)),
+  length(V, I),
+  indent(I),
+  write(C),
+  write('\n'),
+  dfs(C, B, [C|V], P, S1),
+  plus(Sd, S1, S).
+start_dfs(A, B, P, S) :-
+  write(A),
+  write('\n'),
+  dfs(A, B, [A], P, S), !,
+  write('\nPath is: '),
+  write(P),
+  write('\nSum is: '),
+  write(S),
+  length(P, L),
+  write('\nLen is: '),
+  write(L),
+  write('\n').
+
 variant(  1 , murmansk      , odessa          ).
 variant(  2 , st_petersburg , zhitomir        ).
 variant(  3 , samara        , yaroslavl       ).
@@ -57,3 +131,8 @@ variant_number_formula(D, M, V) :-
 my_birthday(4, 5).
 my_variant_number(V) :- my_birthday(D, M), variant_number_formula(D, M, V).
 my_variant(A, B) :- my_variant_number(V), variant(V, A, B).
+
+my_dfs(P, S) :-
+  my_variant(A, B),
+  start_dfs(A, B, P, S).
+
